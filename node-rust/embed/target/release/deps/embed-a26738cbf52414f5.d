af098e40bcfa257cf10939ff42ba61aa94b72525
Rajat Talwar <cuterajat26@gmail.com>
2018-09-09 19:08:01 +0530

    remove target files

--- node-rust/embed/target/release/deps/embed-a26738cbf52414f5.d
+++
@@ -1,5 +0,0 @@
-/Users/rohittalwar/personal/Projects/rust/node-rust/embed/target/release/deps/embed-a26738cbf52414f5: src/main.rs
-
-/Users/rohittalwar/personal/Projects/rust/node-rust/embed/target/release/deps/embed-a26738cbf52414f5.d: src/main.rs
-
-src/main.rs:
--- node-rust/embed/target/release/libdouble_input.d
+++
@@ -1,1 +0,0 @@
-/Users/rohittalwar/personal/Projects/rust/node-rust/embed/target/release/libdouble_input.a: /Users/rohittalwar/personal/Projects/rust/node-rust/embed/src/lib.rs
